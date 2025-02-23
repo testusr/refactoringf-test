2088fc6d6b074e866ddf98d7b6a090167ed48b66
Thorsten Ruehl <thorsten.ruehl.ext@cofinpro.de>
2025-02-23 16:50:10 +0100

    adding should be comment

--- refactoring-test/src/main/java/ssoft/dummy/refs/ReferencingClassA.java
+++ refactoring-test/src/main/java/ssoft/dummy/refs/ReferencingClassA.java
@@ -3,25 +3,53 @@ package ssoft.dummy.refs;
 import ssoft.dummy.beans.MyBeanA;
 
 public class ReferencingClassA {
-    private MyBeanA myBeanA = new MyBeanA();
-    public MyBeanA anotherBeanA = new MyBeanA();
-    MyBeanA andAnotherBeanA = new MyBeanA();
+  private MyBeanA myBeanA = new MyBeanA();
+  public MyBeanA anotherBeanA = new MyBeanA();
+  MyBeanA andAnotherBeanA = new MyBeanA();
 
-    public static void aReference(){
-        var myBeanA = new MyBeanA();
-        myBeanA.aMethodWithoutParameters();
-        myBeanA.setaBoolean(true);
-        myBeanA.aMethodWithParameters(1, 2);
-        myBeanA.setaLong(3L);
-        myBeanA.setaString("hello");
-        System.out.println(myBeanA.aMethodWithoutParameters());
-    }
+  public static void aReference() {
+    var myBeanA = new MyBeanA();
+    myBeanA.aMethodWithoutParameters();
+    myBeanA.setaBoolean(true);
+    myBeanA.aMethodWithParameters(1, 2);
+    myBeanA.setaLong(3L);
+    myBeanA.setaString("hello");
+    System.out.println(myBeanA.aMethodWithoutParameters());
+  }
 
-    MyBeanA doSomethingWithBeans() {
-        this.myBeanA.setaString("helloOO");
-        this.myBeanA.setaBoolean(true);
-        return this.myBeanA;
-    }
-
+  MyBeanA doSomethingWithBeans() {
+    this.myBeanA.setaString("helloOO");
+    this.myBeanA.setaBoolean(true);
+    return this.myBeanA;
+  }
 
 }
+/*
+ * // should be comverted to record usage
+ * public class ReferencingClassA {
+ * private MyBeanA myBeanA = MyBeanA.builder().build();
+ * public MyBeanA anotherBeanA = MyBeanA.builder().build();
+ * MyBeanA andAnotherBeanA = MyBeanA.builder().build();
+ * 
+ * public static void aReference() {
+ * var myBeanA = MyBeanA().builder().build();
+ * myBeanA.aMethodWithoutParameters();
+ * 
+ * myBeanA = myBeanA.toBuilder().setaBoolean(true).build();
+ * myBeanA.aMethodWithParameters(1, 2);
+ * 
+ * myBeanA = myBeanA.toBuilder().withaLong(3L)
+ * .withaString("hello")
+ * .build();
+ * System.out.println(myBeanA.aMethodWithoutParameters());
+ * }
+ * 
+ * MyBeanA doSomethingWithBeans() {
+ * this.myBeanA = this.myBeanA.builder().withaString("helloOO")
+ * .setaBoolean(true)
+ * .build();
+ * return this.myBeanA;
+ * }
+ * 
+ * }
+ */
